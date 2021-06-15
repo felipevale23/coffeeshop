eb5175183c4ce7c965aac96ba7a9071785af426c
Felipe Vale <felipevale@pm.me>
2021-06-15 18:25:55 -0300

    15/06

---
+++ Prospect.java
@@ -0,0 +1,43 @@
+public class Prospect {
+
+    protected double id;
+    protected String fullName;
+    protected double cpf;
+    protected String phone;
+
+    public double getNewID() {
+
+        return 0;
+
+    }
+
+    public void setFullName(String fullName) {
+
+    }
+
+    public String getFullName() {
+
+        return "";
+
+    }
+
+    public void setCPF(double cpf) {
+
+    }
+
+    public double getCPF() {
+
+        return 0;
+    }
+
+    public void setphone(String phone) {
+
+    }
+
+    public String getPhone() {
+
+        return "";
+
+    }
+
+}
