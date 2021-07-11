fb0df9b46af5d5e6287ea0483cd9863a262edf99
root <felipevale@pm.me>
2021-07-11 00:16:10 -0300

    Changes to be committed: 	new file:   cadastro/CriarBotao.java 	new file:   cadastro/TelaCadastro.java
     Changes not staged for commit:
    	deleted:    Cadastro/CriarBotao.java
    	deleted:    Cadastro/TelaCadastro.java
    	modified:   teste/Estoque.java
    	modified:   teste/Main.java
    	modified:   teste/Produto.java

---
+++ cadastro/TelaCadastro.java
@@ -0,0 +1,92 @@
+package cadastro;
+
+import java.awt.Container;
+
+import java.text.ParseException;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
+
+public class TelaCadastro extends JFrame {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String[] args) {
+        TelaCadastro tela = new TelaCadastro();
+        tela.telacadastro();
+
+    }
+
+    public TelaCadastro() {
+        super("Cadastro de Cliente");
+    }
+
+    private void telacadastro() {
+        Container janela = getContentPane();
+        setLayout(null);
+
+        JLabel labelNome = new JLabel("Nome: ");
+
+        JLabel labelCep = new JLabel("Cep: ");
+        JLabel labelTel = new JLabel("Telefone: ");
+        JLabel labelCpf = new JLabel("CPF: ");
+        JLabel labelData = new JLabel("Data: ");
+        labelNome.setBounds(50, 40, 100, 20);
+        labelCep.setBounds(50, 80, 100, 20);
+        labelTel.setBounds(50, 120, 100, 20);
+        labelCpf.setBounds(50, 160, 100, 20);
+        labelData.setBounds(50, 200, 100, 20);
+
+        MaskFormatter mascaraCep = null;
+        MaskFormatter mascaraTel = null;
+        MaskFormatter mascaraCpf = null;
+        MaskFormatter mascaraData = null;
+
+        try {
+            mascaraCep = new MaskFormatter("#####-###");
+            mascaraTel = new MaskFormatter("(##)####-####");
+            mascaraCpf = new MaskFormatter("#########-##");
+            mascaraData = new MaskFormatter("##/##/####");
+            mascaraCep.setPlaceholderCharacter('_');
+            mascaraTel.setPlaceholderCharacter('_');
+            mascaraCpf.setPlaceholderCharacter('_');
+            mascaraData.setPlaceholderCharacter('_');
+
+        } catch (ParseException excp) {
+            System.err.println("Erro na formatacao:" + excp.getMessage());
+            System.exit(-1);
+        }
+        JTextField nome = new JTextField(10);
+        janela.add(nome);
+        JFormattedTextField jFormattedTextCep = new JFormattedTextField(mascaraCep);
+        JFormattedTextField jFormattedTextTel = new JFormattedTextField(mascaraTel);
+        JFormattedTextField jFormattedTextCpf = new JFormattedTextField(mascaraCpf);
+        JFormattedTextField jFormattedTextData = new JFormattedTextField(mascaraData);
+        nome.setBounds(150, 40, 100, 20);
+        jFormattedTextCep.setBounds(150, 80, 100, 20);
+        jFormattedTextTel.setBounds(150, 120, 100, 20);
+        jFormattedTextCpf.setBounds(150, 160, 100, 20);
+        jFormattedTextData.setBounds(150, 200, 100, 20);
+
+        janela.add(labelNome);
+        janela.add(labelCep);
+        janela.add(labelTel);
+        janela.add(labelCpf);
+        janela.add(labelData);
+        janela.add(jFormattedTextCep);
+        janela.add(jFormattedTextTel);
+        janela.add(jFormattedTextCpf);
+        janela.add(jFormattedTextData);
+
+        setSize(500, 350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
