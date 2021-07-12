08bb840fbaccb48907876a80a0afb7d33cb92bce
root <felipevale@pm.me>
2021-07-11 21:05:15 -0300

    new file:   teste/database/estoque_db/Reader.java 	new file:   teste/database/estoque_db/Writer.java 	new file:   teste/database/estoque_db/estoque_db.txt 	new file:   teste/database/estoque_db/tip.txt 	new file:   teste/estoque/Estoque.java 	new file:   teste/estoque/Main.java 	new file:   teste/estoque/Produto.java 	new file:   teste/estoque/estoque_db.txt 	new file:   teste/prospect/Client.java 	new file:   teste/prospect/ExemploCPF.java 	new file:   teste/prospect/Prospect.java 	new file:   teste/prospect/ValidaCPF.java

---
+++ teste/database/estoque_db/Reader.java
@@ -0,0 +1,49 @@
+package teste.database.estoque_db;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Reader {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileInputStream estoque_db = new FileInputStream("estoque_db.txt");
+            InputStreamReader input = new InputStreamReader(estoque_db);
+            BufferedReader reader = new BufferedReader(input);
+
+            String s;
+
+            do {
+
+                s = reader.readLine();
+
+                if (s != null) {
+
+                    String[] w = s.split(";");
+
+                    System.out.println("-----------------------------------------");
+
+                    for (int i = 0; i < w.length; i++) {
+
+                        System.out.println("Palavra Lida = " + w[i]);
+
+                    }
+
+                }
+
+            } while (s != null);
+
+            reader.close();
+
+        } catch (Exception e) {
+
+            System.out.println("");
+
+        }
+
+    }
+
+}
---
+++ teste/prospect/ExemploCPF.java
@@ -0,0 +1,29 @@
+package teste.prospect;
+
+import java.util.Scanner;
+
+public class ExemploCPF {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String cpf;
+
+		System.out.println("Informe um CPF: ");
+		cpf = sc.next();
+
+		System.out.println("\nResultado: ");
+
+		if (ValidaCPF.isCPF(cpf)) {
+
+			System.out.printf("%s\n", ValidaCPF.imprimeCPF(cpf));
+
+		} else {
+
+			System.out.println("Erro CPf invalido");
+		}
+
+		sc.close();
+
+	}
+
+}
